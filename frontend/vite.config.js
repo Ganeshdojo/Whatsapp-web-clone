d9e280d8109d366a915d0a8da3fbe150e2ab1ce2
Gangadhar <pganesh0067@gmail.com>
2025-08-15 08:51:59 +0530

    Updated vite config file for deploying

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: "terser",
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
 });
